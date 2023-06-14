24133fe0eb69cbee46b10b28b8ede0cd5e81f1d4
Sherina Buenaseda <sherina76@gmail.com>
2023-06-14 16:48:07 -0600

    committing

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+let mondayWork = function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+
+let wrapAdjective = function(kudos = "*"){
+    return function(kudos2 = "special"){
+        return `You are ${kudos}${kudos2}${kudos}!`
+        }
+}
